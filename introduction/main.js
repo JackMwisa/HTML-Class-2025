439ed97a5566df6ce8520f349ab14835b6d7974c
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-03-14 12:36:19 +0300

    Update main.js

--- introduction/main.js
+++ introduction/main.js
@@ -1,2 +1,14 @@
 // Alert saying 'Hi' to the user
 alert('Hi');
+
+let score = 90;
+let score2 = 95;
+let score3 = 85;
+let score4 = 80;
+
+let sum = score + score2 + score3 + score4;
+let average = sum / 4;
+
+console.log(sum);
+console.log(average);
+
